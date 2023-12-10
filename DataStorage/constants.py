a3d516a0d68d2b6a5c2e2e2d54d7b2ad59d0ea17
christinathr2000 <christina@thrainer.at>
2023-12-10 21:17:02 +0100

    Added Alphapose and DataStorage

---
+++ DataStorage/constants.py
@@ -0,0 +1,3 @@
+access_key = 'AKIAYM2VOSRCA6TE43N2'
+secret_access_key = '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'
+bucket_name = 'ivu-cloud'
---
+++ DataStorage/main.py
@@ -0,0 +1,76 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
+import constants
+import requests
+
+def generate_download_url(bucket_name, object_name, expiration=10):
+    """Generate a presigned URL for the S3 object."""
+    s3_client = boto3.client(
+        's3',
+    aws_access_key_id=constants.access_key,
+    aws_secret_access_key=constants.secret_access_key,
+    )
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                   Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                   ExpiresIn=expiration)
+    except NoCredentialsError:
+        print("Credentials not available")
+        return None
+    return response
+
+
+# def generate_upload_url(bucket_name, object_name,content_type='image/png', expiration=10):
+#     """Generate a presigned URL to upload a file."""
+#     s3_client = boto3.client(
+#         's3',
+#         aws_access_key_id=constants.access_key,
+#         aws_secret_access_key=constants.secret_access_key,
+#     )
+#     try:
+#         response = s3_client.generate_presigned_url('put_object',
+#                                                     Params={'Bucket': bucket_name,
+#                                                             'Key': object_name,
+#                                                             'ContentType': content_type},
+#                                                     ExpiresIn=expiration)
+#     except Exception as e:
+#         print(e)
+#         return None
+#     return response
+
+
+upload_file_name = 'RTG.mp4'
+download_file_name = 'RTG.mp4'
+
+# upload_url = generate_upload_url(constants.bucket_name, upload_file_name)
+# print("Upload URL:", upload_url)
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=constants.access_key,
+    aws_secret_access_key=constants.secret_access_key,
+)
+
+response = s3_client.generate_presigned_post(
+    Bucket=constants.bucket_name,
+    Key=upload_file_name,
+    ExpiresIn=10
+)
+
+print(response['url'])
+
+files = {'file': open(upload_file_name, 'rb')}
+r = requests.post(response['url'], data=response['fields'], files=files)
+print(r.status_code)
+
+url = generate_download_url(constants.bucket_name, download_file_name, expiration=10)
+print(url)
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    with open(f'downloaded_{download_file_name}', 'wb') as file:
+        file.write(response.content)
+else:
+    print("Failed to download the file")
